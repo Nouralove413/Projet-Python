2d06bb2a33e775c908fed73e1f23c4e8aa845165
Bayane <235305613+bayane34@users.noreply.github.com>
2026-02-17 11:49:59 +0100

    Update class_utilisateur.py

--- class_utilisateur.py
+++ class_utilisateur.py
@@ -1,22 +1,27 @@
 class Emprunt:
+    # initialisation
     def __init__(self, adherents, livres):
         self.adherents = adherents
         self.livres = livres
 
     def emprunter(self, num_adherent, titre):
+        # vérifie si l'adhérent existe
         if num_adherent not in self.adherents:
             print("Adhérent non trouvé, vous n'êtes pas inscrit à la bibliothèque.")
             return
 
         adherent = self.adherents[num_adherent]
 
-        # Vérifie le livre
+        # vérifie la disponibilité du livre
         for isbn, livre in self.livres.items():
             if livre['titre'] == titre:
+                # si le nombre d'exemplaires est "inférieur" ou égal à 0 il n'est pas disponible
                 if livre['nb exemplaires'] <= 0:
                     print("Le livre n'est pas disponible actuellement.")
                     return
 
+                # len permet de compter le nombre d'éléments (ici une liste)
+                # on compte le nombre de livres empruntés et si c'est supérieur ou égal à 5, l'adhérent ne peut plus emprunter
                 if len(adherent['livres_empruntés']) >= 5:
                     print("L'adhérent a emprunté son quota maximum de livres.")
                     return
@@ -29,7 +34,7 @@ class Emprunt:
         print("Ce livre n'est pas dans la bibliothèque.")
 
     def rendre(self, num_adherent, titre):
-        
+        # vérifie si l'utilisateur existe 
         if num_adherent not in self.adherents:
             print("Adhérent non trouvé.")
             return
@@ -42,7 +47,7 @@ class Emprunt:
 
         adherent['livres_empruntés'].remove(titre)
 
-        # Permet de rendre un livre 
+        # permet de "rendre" le livre dans l'inventaire 
         for isbn, livre in self.livres.items():
             if livre['titre'] == titre:
                 livre['nb exemplaires'] += 1 
